6319f69808298e1a652211f6af01480854018a6e
Dominik Rimpf <dev@d-rimpf.de>
2020-07-23 22:02:33 +0200

    Fixed leveling and z-offset. Optimized sensorless homing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -998,7 +998,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -45.75, -10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -45.75, -10, -1.750 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1303,7 +1303,7 @@
 
     // Beyond the probed grid, continue the implied tilt?
     // Default is to maintain the height of the nearest edge.
-    #define EXTRAPOLATE_BEYOND_GRID
+    //#define EXTRAPOLATE_BEYOND_GRID
 
     //
     // Experimental Subdivision of the grid by Catmull-Rom method.
