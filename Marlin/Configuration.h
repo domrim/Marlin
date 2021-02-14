8083b59efc2fa8bcd0aa221e6274a9e7cd3d9563
Dominik Rimpf <dev@drimpf.de>
2021-02-14 23:47:49 +0100

    CHG: Pitan extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -899,7 +899,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 95.5 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 400 }
 
 /**
  * Default Max Feed Rate (mm/s)
